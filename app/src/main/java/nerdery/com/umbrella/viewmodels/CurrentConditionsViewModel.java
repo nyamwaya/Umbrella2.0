cf13c96d2bd7200484c0cd9623449698792a8665
nyamwaya <alecksonnyamwaya@gmail.com>
2016-08-16 16:09:10 -0500

    create viewmodel package

---
+++ app/src/main/java/nerdery/com/umbrella/viewmodels/CurrentConditionsViewModel.java
@@ -0,0 +1,13 @@
+package nerdery.com.umbrella.viewmodels;
+
+import android.databinding.BaseObservable;
+
+/**
+ * This class is a view model. It acts as "glue"
+ * in-between the view and the model using data binding
+ *
+ * @author  aleckson
+ */
+
+public class CurrentConditionsViewModel extends BaseObservable {
+}
